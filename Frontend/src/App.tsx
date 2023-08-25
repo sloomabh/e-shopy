7e8a292f790da86a7c4845be18f0ffe509a07a98
pej.benhalima <pej.benhalima@gmail.com>
2023-08-25 17:58:01 +0900

    playing arround with styles

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,14 @@
-import "./App.css";
-
 function App() {
-  return <div className="App">salimoo</div>;
+  return (
+    <div className="h-16  bg-zinc-500">
+      <div className="h-16   inline-block bg-white  text-xl  text-black-600 font-bold border-4 border-salim max-w-sm">
+        Hello world! dwdwdwdwwwwwcss
+      </div>
+      <div className="h-16  bg-white  text-xl  text-black-600 font-bold border-4 border-salim ">
+        Hello world! ss
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
